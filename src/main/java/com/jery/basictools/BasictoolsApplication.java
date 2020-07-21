3429aa28b7f1face343912c650c61da967c912d0
litaiping_jp@lakala.com <litaiping_jp@lakala.com>
2020-07-21 10:50:54 +0800

    first commit

---
+++ src/main/java/com/jery/basictools/BasictoolsApplication.java
@@ -0,0 +1,13 @@
+package com.jery.basictools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasictoolsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasictoolsApplication.class, args);
+    }
+
+}
